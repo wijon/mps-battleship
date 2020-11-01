9c8b6f19b6327fdee1b54e28bfd8f75ffaa4d40b
Jonas Weber <jonas.weber@hermle.de>
2020-11-01 21:55:13 +0100

    Add UnitTests for OutputHelper Add Methods for hit and destroyed ship info text at OutputHelper

--- src/test/scala/model/OutputHelperSpec.scala
+++ src/test/scala/model/OutputHelperSpec.scala
@@ -4,33 +4,99 @@ import org.scalatest.wordspec.AnyWordSpec
 
 class OutputHelperSpec extends AnyWordSpec {
   "Victory view" when {
-    "shown" should {
+    "called" should {
+      val textAsString = OutputHelper.generateVictory().mkString(" ")
+
       "not be empty" in {
-        assert(!OutputHelper.generateVictory().isEmpty)
+        assert(!textAsString.isEmpty)
       }
 
       "contain victory text" in {
-        assert(OutputHelper.generateVictory().mkString(" ").contains("SIE HABEN GEWONNEN"))
+        assert(textAsString.contains("SIE HABEN GEWONNEN"))
       }
 
       "contain a happy face" in {
-        assert(OutputHelper.generateVictory().mkString(" ").contains(":)"))
+        assert(textAsString.contains(":)"))
       }
     }
   }
 
   "Loss view" when {
-    "shown" should {
+    "called" should {
+      val textAsString = OutputHelper.generateLoss().mkString(" ")
+
       "not be empty" in {
-        assert(!OutputHelper.generateLoss().isEmpty)
+        assert(!textAsString.isEmpty)
       }
 
       "contain victory text" in {
-        assert(OutputHelper.generateLoss().mkString(" ").contains("SIE HABEN VERLOREN"))
+        assert(textAsString.contains("SIE HABEN VERLOREN"))
       }
 
       "contain a sad face" in {
-        assert(OutputHelper.generateLoss().mkString(" ").contains(":("))
+        assert(textAsString.contains(":("))
+      }
+    }
+  }
+
+  "Round info text view" when {
+    "called" should {
+      val roundNumber = 53
+      val textAsString = OutputHelper.generateRoundInfoText(roundNumber).mkString(" ")
+
+      "not be empty" in {
+        assert(!textAsString.isEmpty)
+      }
+
+      "contain round number" in {
+        val regex = "^.*(Rundennummer: " + roundNumber + ").*$"
+        assert(textAsString.matches(regex))
+      }
+    }
+  }
+
+  "Ai info text" when {
+    "called" should {
+      val testCoordinates = new Coordinates(3, 7)
+      val textAsString = OutputHelper.generateAiInfoText(testCoordinates).mkString(" ")
+
+      "not be empty" in {
+        assert(!textAsString.isEmpty)
+      }
+
+      "contain the coordinates" in {
+        val regex = "^.*" + testCoordinates.x + testCoordinates.y + ".*$"
+        assert(textAsString.matches(regex))
+      }
+    }
+  }
+
+  "Ship hit info text" when {
+    "called" should {
+      val testShip = new Ship(3, "TestShip")
+      val textAsString = OutputHelper.generateShipHitInfotext(testShip).mkString(" ")
+
+      "not be empty" in {
+        assert(!textAsString.isEmpty)
+      }
+
+      "contain ship name" in {
+        assert(textAsString.contains(testShip.name))
+      }
+    }
+  }
+
+  "Ship destroyed info text" when {
+    "called" should {
+      val testShip = new Ship(3, "TestShip")
+      val textAsString = OutputHelper.generateShipDestroyedInfoText(testShip).mkString(" ")
+
+      "not be empty" in {
+        assert(!textAsString.isEmpty)
+      }
+
+      "contain ship name" in {
+        assert(textAsString.contains(testShip.name))
       }
     }
   }
