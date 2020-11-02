ede56632e30c150cb592646de7b5a6b8773d5cc6
Jonas Weber <jonas.weber@hermle.de>
2020-11-02 17:44:23 +0100

    Add unit tests for remaining ships info text

--- src/test/scala/model/OutputHelperSpec.scala
+++ src/test/scala/model/OutputHelperSpec.scala
@@ -101,4 +101,120 @@ class OutputHelperSpec extends AnyWordSpec {
       }
     }
   }
+
+  "Remaining ship info text" when {
+    "called with multiple ships on the board" should {
+      val ships = Vector(
+        Ship(2, "Test 1"),
+        Ship(3, "Test 2")
+      )
+      val matrix = Vector.tabulate(10, 10) { (_, _) => BoardCell(false) }
+      val shipPositions = Vector(
+        ShipPosition(ships(0), Vector(Coordinates(3, 4), Coordinates(3, 5))),
+        ShipPosition(ships(1), Vector(Coordinates(1,1), Coordinates(1,2), Coordinates(1,3)))
+      )
+
+      val matrix1 = matrix.updated(3, matrix(3).updated(4, BoardCell(true)))
+      val matrix2 = matrix1.updated(3, matrix1(3).updated(5, BoardCell(true)))
+      val matrix3 = matrix2.updated(1, matrix2(1).updated(2, BoardCell(true)))
+      val testBoard = Board(matrix3, ships, shipPositions)
+
+      val test = OutputHelper.generateRemainingShips(testBoard);
+
+      "contain one line for every ship on the board" in {
+        assert(test.length == ships.length)
+      }
+    }
+
+    "called with one ship on the board, that is not hit" should {
+      val shipName = "test 123"
+
+      val ships = Vector(
+        Ship(2, shipName)
+      )
+      val matrix = Vector.tabulate(10, 10) { (_, _) => BoardCell(false) }
+      val shipPositions = Vector(
+        ShipPosition(ships(0), Vector(Coordinates(3, 4), Coordinates(3, 5))),
+      )
+
+      val testBoard = Board(matrix, ships, shipPositions)
+
+      val test = OutputHelper.generateRemainingShips(testBoard);
+
+      "show this ship correctly" in {
+        assert(test.mkString(" ").contains("\\__/"))
+      }
+
+      "show the name of the ship" in {
+        assert(test.mkString(" ").contains(shipName))
+      }
+
+      "show 0 hits" in {
+        assert(test.mkString(" ").contains("0 hit(s)"))
+      }
+    }
+
+    "called with one hit ship on the board" should {
+      val shipName = "test 123"
+
+      val ships = Vector(
+        Ship(2, shipName)
+      )
+      val matrix = Vector.tabulate(10, 10) { (_, _) => BoardCell(false) }
+      val shipPositions = Vector(
+        ShipPosition(ships(0), Vector(Coordinates(3, 4), Coordinates(3, 5))),
+      )
+
+      val matrix1 = matrix.updated(3, matrix(3).updated(5, BoardCell(true)))
+      val testBoard = Board(matrix1, ships, shipPositions)
+
+      val test = OutputHelper.generateRemainingShips(testBoard);
+
+      "show this ship correctly" in {
+        assert(test.mkString(" ").contains("\\_X/"))
+      }
+
+      "show the name of the ship" in {
+        assert(test.mkString(" ").contains(shipName))
+      }
+
+      "show 1 hit" in {
+        assert(test.mkString(" ").contains("1 hit(s)"))
+      }
+    }
+
+    "called with one destroyed ship on the board" should {
+      val shipName = "test 123"
+
+      val ships = Vector(
+        Ship(2, shipName)
+      )
+      val matrix = Vector.tabulate(10, 10) { (_, _) => BoardCell(false) }
+      val shipPositions = Vector(
+        ShipPosition(ships(0), Vector(Coordinates(3, 4), Coordinates(3, 5))),
+      )
+
+      val matrix1 = matrix.updated(3, matrix(3).updated(4, BoardCell(true)))
+      val matrix2 = matrix1.updated(3, matrix1(3).updated(5, BoardCell(true)))
+      val testBoard = Board(matrix2, ships, shipPositions)
+
+      val test = OutputHelper.generateRemainingShips(testBoard);
+
+      "show this ship correctly" in {
+        assert(test.mkString(" ").contains("\\XX/"))
+      }
+
+      "show the name of the ship" in {
+        assert(test.mkString(" ").contains(shipName))
+      }
+
+      "show 2 hits" in {
+        assert(test.mkString(" ").contains("2 hit(s)"))
+      }
+
+      "show destroyed" in {
+        assert(test.mkString(" ").contains("destroyed"))
+      }
+    }
+  }
 }
