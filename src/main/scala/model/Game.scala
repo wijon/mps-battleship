8043d388b906c7168ca58cf0ed137fb579330929
Jonas Weber <jonas.weber@hermle.de>
2020-11-08 20:45:38 +0100

    Add game methods with unitTests
    isRunning()
    humanPlayerIsWinner()

--- src/main/scala/model/Game.scala
+++ src/main/scala/model/Game.scala
@@ -10,4 +10,22 @@ case class Game(player1Board: Board, player2Board: Board, roundNum: Int) {
   // Override constructor, this one is used for the initial instantiation
   def this(player1Ships: Vector[Ship], player2Ships: Vector[Ship]) =
     this(new Board(player1Ships), new Board(player2Ships), 0)
+
+    /** Check if all Ships of one Player are destroyed
+     *
+     * @return Game still running?
+     */
+    def isRunning(): Boolean = {
+      !player1Board.ships.forall(player1Board.isDestroyed(_)) &&
+        !player2Board.ships.forall(player2Board.isDestroyed(_))
+    }
+
+    /** Check if human player is winner
+     *
+     * @return Winner?
+     */
+    def humanPlayerIsWinner(): Boolean = {
+      !player1Board.ships.forall(player1Board.isDestroyed(_)) &&
+        player2Board.ships.forall(player2Board.isDestroyed(_))
+    }
 }
--- src/test/scala/model/GameSpec.scala
+++ src/test/scala/model/GameSpec.scala
@@ -19,5 +19,105 @@ class GameSpec extends AnyWordSpec {
         assert(game.player2Board.ships == p2Ships)
       }
     }
+
+    "all ships of human player are destroyed" should {
+      val shipsHuman = Vector(
+        Ship(2, "humanShip1")
+      )
+      val shipsAi = Vector(
+        Ship(2, "aiShip1")
+      )
+
+      val shipPositionsHuman = Vector(
+        ShipPosition(shipsHuman(0), Vector(Coordinates(3, 4), Coordinates(3, 5))),
+      )
+
+      val shipPositionsAi = Vector(
+        ShipPosition(shipsAi(0), Vector(Coordinates(3, 4), Coordinates(3, 5))),
+      )
+
+      val matrixHuman = Vector.tabulate(10, 10) { (_, _) => BoardCell(false) }
+      val matrixHuman2 = matrixHuman.updated(3, matrixHuman(3).updated(4, BoardCell(true)))
+      val matrixHuman3 = matrixHuman2.updated(3, matrixHuman2(3).updated(5, BoardCell(true)))
+
+      val matrixAi = Vector.tabulate(10, 10) { (_, _) => BoardCell(false) }
+
+      val testBoardHuman = Board(matrixHuman3, shipsHuman, shipPositionsHuman)
+      val testBoardAi = Board(matrixAi, shipsAi, shipPositionsAi)
+
+      val testGame = Game(testBoardHuman, testBoardAi, 10)
+
+      "not be running" in {
+        assert(!testGame.isRunning())
+      }
+
+      "declare ai as winner" in {
+        assert(!testGame.humanPlayerIsWinner())
+      }
+    }
+
+    "all ships of ai player are destroyed" should {
+      val shipsHuman = Vector(
+        Ship(2, "humanShip1")
+      )
+      val shipsAi = Vector(
+        Ship(2, "aiShip1")
+      )
+
+      val shipPositionsHuman = Vector(
+        ShipPosition(shipsHuman(0), Vector(Coordinates(3, 4), Coordinates(3, 5))),
+      )
+
+      val shipPositionsAi = Vector(
+        ShipPosition(shipsAi(0), Vector(Coordinates(3, 4), Coordinates(3, 5))),
+      )
+
+      val matrixHuman = Vector.tabulate(10, 10) { (_, _) => BoardCell(false) }
+      val matrixAi = Vector.tabulate(10, 10) { (_, _) => BoardCell(false) }
+      val matrixAi2 = matrixAi.updated(3, matrixAi(3).updated(4, BoardCell(true)))
+      val matrixAi3 = matrixAi2.updated(3, matrixAi2(3).updated(5, BoardCell(true)))
+
+      val testBoardHuman = Board(matrixHuman, shipsHuman, shipPositionsHuman)
+      val testBoardAi = Board(matrixAi3, shipsAi, shipPositionsAi)
+
+      val testGame = Game(testBoardHuman, testBoardAi, 10)
+
+      "not be running" in {
+        assert(!testGame.isRunning())
+      }
+
+      "declare human as winner" in {
+        assert(testGame.humanPlayerIsWinner())
+      }
+    }
+
+    "both players have at least one not-destroyed ship" should {
+      val shipsHuman = Vector(
+        Ship(2, "humanShip1")
+      )
+      val shipsAi = Vector(
+        Ship(2, "aiShip1")
+      )
+
+      val shipPositionsHuman = Vector(
+        ShipPosition(shipsHuman(0), Vector(Coordinates(3, 4), Coordinates(3, 5))),
+      )
+
+      val shipPositionsAi = Vector(
+        ShipPosition(shipsAi(0), Vector(Coordinates(3, 4), Coordinates(3, 5))),
+      )
+
+      val matrixHuman = Vector.tabulate(10, 10) { (_, _) => BoardCell(false) }
+      val matrixAi = Vector.tabulate(10, 10) { (_, _) => BoardCell(false) }
+
+      val testBoardHuman = Board(matrixHuman, shipsHuman, shipPositionsHuman)
+      val testBoardAi = Board(matrixAi, shipsAi, shipPositionsAi)
+
+      val testGame = Game(testBoardHuman, testBoardAi, 10)
+
+      "be running" in {
+        assert(testGame.isRunning())
+      }
+    }
   }
 }
